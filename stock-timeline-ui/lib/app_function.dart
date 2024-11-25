d2b50c5c8c82e6bbd3069886155c8ba37a294550
hiblue02 <hiblue02@gmail.com>
2024-11-25 23:02:55 +0900

    수정: 엑셀 다운로드 기능을 excel 라이브러리로 바꾼다

--- stock-timeline-ui/lib/app_function.dart
+++ stock-timeline-ui/lib/app_function.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'dart:typed_data';
-import 'package:path_provider/path_provider.dart';
-import 'package:excel/excel.dart';
+
 import 'package:file_picker/file_picker.dart';
 
 Future<Uint8List?> uploadExcel() async {
@@ -12,38 +10,7 @@ Future<Uint8List?> uploadExcel() async {
 
   if (result == null || result.files.isEmpty) {
     throw Exception("파일이 선택되지 않았습니다.");
-
   }
 
   return result.files.single.bytes;
-}
-
-Future<void> downloadExcel() async {
-  // 엑셀 파일 생성
-  var excel = Excel.createExcel();
-  Sheet sheet = excel['Sheet1'];
-
-  // 샘플 데이터
-  List<List<String>> data = [
-    ['제목', 'X축 이름', 'Y축 이름'],
-    ['월별 판매량', '월', '판매량'],
-    ['주별 방문자 수', '주', '방문자 수'],
-    ['연도별 수익', '연도', '수익'],
-    ['분기별 성장률', '분기', '성장률'],
-    ['분야별 점유율', '분야', '점유율'],
-    ['연도별 비용', '연도', '비용'],
-  ];
-
-  // 데이터 추가
-  for (var row in data) {
-    sheet.appendRow(row);
-  }
-
-  // Excel 파일을 Uint8List로 변환
-  var bytes = excel.encode();
-
-  final directory = await getApplicationDocumentsDirectory();
-  final path = "${directory.path}/data.xlsx";
-  final file = File(path);
-  await file.writeAsBytes(bytes!);
 }
